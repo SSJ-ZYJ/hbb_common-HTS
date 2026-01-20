d060eabe5048cddece7930f7295fdb1be5f2d1b2
Shenshijun <hisos_shenshijun@outlook.com>
2026-01-20 22:52:29 +0800

    modify hbb_common modules

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["101.43.13.70"];
+pub const RS_PUB_KEY: &str = "FcVfawXzG0NpcHXdLnWyjk0kvG43wcgP1Hj3yPyx8a8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
